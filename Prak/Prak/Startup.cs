00d8763614bf41cecdc5c25124bbb2b27528ff10
Вадим <@VADIM>
2016-09-06 17:51:27 +0700

    sdfgtot

---
+++ Prak/Prak/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prak.Startup))]
+namespace Prak
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
