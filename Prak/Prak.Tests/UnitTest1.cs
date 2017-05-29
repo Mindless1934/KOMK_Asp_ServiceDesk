3249867424ddaa25e962163d1e0c4268af37e2b8
Вадим <@VADIM>
2017-05-29 18:57:42 +0700

    Фикс кнопок/ Начало тестов

---
+++ Prak/Prak.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prak.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
