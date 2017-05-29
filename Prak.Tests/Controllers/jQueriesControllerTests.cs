c9b678ea7ffbd6be503fa7149c4fec298ee9b574
Вадим <@VADIM>
2017-05-30 03:09:53 +0700

    Попробуем положить уже на тревис

---
+++ Prak.Tests/Controllers/jQueriesControllerTests.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prak.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using static Prak.Controllers.jQueriesController;
+using System.Web.Mvc;
+using Microsoft.CSharp;
+
+
+namespace Prak.Tests
+{
+    [TestClass()]
+    public class jQueriesControllerTests
+    {
+        jQueriesController jQ;
+
+        //Простенький тест для проверки работы метода
+        [TestMethod()]
+        public void MyPowTest()
+        {
+            jQ = new jQueriesController();
+            Assert.AreEqual(4, jQ.MyPow(2));
+        }
+
+        //Тест для проверки информации передающейся на представление
+        [TestMethod()]
+        public void SomeTextTest()
+        {
+            jQ = new jQueriesController();
+            //ViewResult result = jQ.SomeText() as ViewResult;
+            //string actual = result.ViewBag.Message as string;
+            //Assert.AreEqual("Your application description page.", actual);
+            var result = jQ.SomeText();
+            Assert.AreEqual("SomeText", ((ViewResult)result).ViewName);
+        }
+
+        //Проверим логику поиска Администраторов в системе
+        [TestMethod()]
+        public void GetAdminTest()
+        {
+            //Создадим пользователей
+            AspNetUsers user1 = new AspNetUsers();
+            user1.Id = "Id1";
+            AspNetUsers user2 = new AspNetUsers();
+            user1.Id = "Id2";
+            List<AspNetUsers> users = new List<AspNetUsers> { user1, user2 };
+
+            //Создадим роли
+            AspNetRoles admin = new AspNetRoles();
+            admin.Name = "Admin";
+            admin.Id = "admin1";
+            AspNetRoles user = new AspNetRoles();
+            user.Name = "User";
+            user.Id = "user1";
+            List<AspNetRoles> rols = new List<AspNetRoles> { admin, user };
+
+            //Распределим роли по пользователям
+            AspNetUserRoles userRol1 = new AspNetUserRoles();
+            userRol1.UserRoleId = 1;
+            userRol1.UserId = "Id1";
+            userRol1.RoleId = "admin1";
+            AspNetUserRoles userRol2 = new AspNetUserRoles();
+            userRol2.UserRoleId = 2;
+            userRol2.UserId = "Id2";
+            userRol2.RoleId = "user1";
+            List<AspNetUserRoles> userRols = new List<AspNetUserRoles> { userRol1, userRol2 };
+
+            //Чтобы наши тесты работали без использования бд воспользуемся Фреймворком Moq
+            var mock = new Mock<IRepository>();
+            //Заменим используемые для работы с бд методы, сразу готовыми значениями
+            mock.Setup(a => a.GetUserList()).Returns(users);
+            mock.Setup(b => b.GetRoleList()).Returns(rols);
+            mock.Setup(c => c.GetUserRoleList()).Returns(userRols);
+            mock.Setup(d => d.GetUserFromDb("Id1")).Returns(user1);
+            mock.Setup(d => d.GetUserFromDb("Id2")).Returns(user2);
+            jQ = new jQueriesController(mock.Object);
+            //Администратора мы сделали только 1 по этому вернуться должно 1
+            Assert.AreEqual(1, jQ.GetAdmin().Count);
+        }
+
+        //Проверим логику поиска Администраторов в системе когда их нет
+        [TestMethod()]
+        public void GetAdminZeroTest()
+        {
+
+            AspNetUsers user1 = new AspNetUsers();
+            user1.Id = "Id1";
+            AspNetUsers user2 = new AspNetUsers();
+            user1.Id = "Id2";
+            List<AspNetUsers> users = new List<AspNetUsers> { user1, user2 };
+
+
+            AspNetRoles admin = new AspNetRoles();
+            admin.Name = "Admin";
+            admin.Id = "admin1";
+            AspNetRoles user = new AspNetRoles();
+            user.Name = "User";
+            user.Id = "user1";
+            List<AspNetRoles> rols = new List<AspNetRoles> { admin, user };
+
+
+            AspNetUserRoles userRol1 = new AspNetUserRoles();
+            userRol1.UserRoleId = 1;
+            userRol1.UserId = "Id1";
+            userRol1.RoleId = "user1"; //назначим всех простыми юзерами
+            AspNetUserRoles userRol2 = new AspNetUserRoles();
+            userRol2.UserRoleId = 2;
+            userRol2.UserId = "Id2";
+            userRol2.RoleId = "user1";
+            List<AspNetUserRoles> userRols = new List<AspNetUserRoles> { userRol1, userRol2 };
+
+            var mock = new Mock<IRepository>();
+            mock.Setup(a => a.GetUserList()).Returns(users);
+            mock.Setup(b => b.GetRoleList()).Returns(rols);
+            mock.Setup(c => c.GetUserRoleList()).Returns(userRols);
+            mock.Setup(d => d.GetUserFromDb("Id1")).Returns(user1);
+            mock.Setup(d => d.GetUserFromDb("Id2")).Returns(user2);
+            jQ = new jQueriesController(mock.Object);
+            //В системе нет Администраторов ожидаем ответ 0
+            Assert.AreEqual(0, jQ.GetAdmin().Count);
+        }
+
+
+    }
+}
---
+++ Prak/Controllers/jQueriesController.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Prak.Models;
+using Microsoft.AspNet.Identity;
+using MvcSiteMapProvider;
+
+
+namespace Prak.Controllers
+{
+    //Контроллер отвечает за обработку входящих запросов, 
+    //выполнение операций над моделью предметной области и выбор представлений для визуализации пользователю.
+   // [MvcSiteMapNode(Title = "TestGlobal", ParentKey = "Home", Key = "jQuery", Roles = new string[] { "User" })]
+    public class jQueriesController : Controller
+    {
+        //Создаем экземпляр класса контекста для взаимодействия с нашей бд 
+        private KOMK_Main_v2Entities db = new KOMK_Main_v2Entities();
+
+        public interface IRepository : IDisposable
+        {
+            List<AspNetUsers> GetUserList();
+            List<AspNetUserRoles> GetUserRoleList();
+            List<AspNetRoles> GetRoleList();
+            AspNetUsers GetUserFromDb(string Id);
+
+        }
+
+        class Mydb : IRepository
+        {
+            private KOMK_Main_v2Entities db;
+
+            public  Mydb()
+            {
+                this.db = new KOMK_Main_v2Entities();
+            }
+            public List<AspNetUsers> GetUserList()
+            {
+                return db.AspNetUsers.ToList();
+            }
+
+            public List<AspNetUserRoles> GetUserRoleList()
+            {
+                return db.AspNetUserRoles.ToList();
+            }
+
+            public List<AspNetRoles> GetRoleList()
+            {
+                return db.AspNetRoles.ToList();
+            }
+
+            public AspNetUsers GetUserFromDb(string Id)
+            {
+                return db.AspNetUsers.Find(Id);
+            }
+
+
+            private bool disposed = false;
+
+            public virtual void Dispose(bool disposing)
+            {
+                if (!this.disposed)
+                {
+                    if (disposing)
+                    {
+                        db.Dispose();
+                    }
+                }
+                this.disposed = true;
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        IRepository repo;
+
+        public jQueriesController(IRepository r)
+        {
+            repo = r;
+        }
+        public jQueriesController()
+        {
+            repo = new Mydb();
+        }
+        // GET: jQueries
+        // Метод Index, в нем мы задаем связи с какими таблицами нам будут нужны для оботражения данных в представлени
+        //Результатом работы метода является вызов представления Index    
+        //[MvcSiteMapNode(Title = "Test", ParentKey = "jQuery", Key = "jQueryIndex", Roles = new string[] { "User" })]   
+        public ActionResult Index()
+        {
+            var jQuery = db.jQuery.Include(j => j.AspNetUsers).Include(j => j.AspNetUsers1).Include(j => j.hState);
+            AspNetUsers userNow = db.AspNetUsers.Find(User.Identity.GetUserId());
+            AspNetUserRoles usrol = db.AspNetUserRoles.Where(m => m.UserId == userNow.Id).First();
+            AspNetRoles rol = db.AspNetRoles.Where(m => m.Id == usrol.RoleId).First();
+            switch (rol.Name)
+            {
+                case "Admin": return View(jQuery.ToList());
+                case "User": return View(jQuery.Where(m=>m.PersonId == userNow.Id).ToList());
+            }
+            return View(jQuery.ToList());
+        }
+
+        // GET: jQueries/Details/5
+        // Метод Детали, имеет аргумент id, чтобы получать из базы данные только по 1 конкретной заявке
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Производим поиск по id
+            jQuery jQuery = db.jQuery.Find(id);
+            if (jQuery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jQuery);
+        }
+
+        public List<AspNetUsers> GetAdmin()
+        {
+            List<AspNetUserRoles> usrol = repo.GetUserRoleList().Where(m => m.RoleId == repo.GetRoleList().Where(r => r.Name =="Admin").FirstOrDefault().Id).ToList();
+            List<AspNetUsers> users = new List<AspNetUsers>();
+            foreach(AspNetUserRoles ur in usrol)
+            {
+                users.Add(repo.GetUserFromDb(ur.UserId));
+            }
+            return users;
+        }
+        // GET: jQueries/Create
+        // Используем ViewBag для того чтобы в представлении у нас были вместо вторичных ключей конкретные значения из связаных таблиц
+        public ActionResult Create()
+        {
+            ViewBag.PersonId = new SelectList(db.AspNetUsers, "Id", "Fio");
+            ViewBag.PersonSpId = new SelectList(GetAdmin(), "Id", "Fio");
+            ViewBag.StateId = new SelectList(db.hState, "StateId", "Description");
+            return View();
+        }
+
+        // POST: jQueries/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost] //Обрабатываем данные полученые из представления
+        [ValidateAntiForgeryToken] 
+        public ActionResult Create([Bind(Include = "QueryId,DateOut,DateIn,DateModification,DeadLine,Text,StateId,PersonId,PersonSpId")] jQuery jQuery)
+        {
+            if (ModelState.IsValid)
+            {
+                //Заполняем необходимые для для создания заявки поля, которые не видит пользователь 
+                jQuery.DateIn = DateTime.Parse(DateTime.Today.ToShortDateString());
+                jQuery.DateModification = DateTime.Now;
+                jQuery.DateModification = jQuery.DateModification.AddMilliseconds(-jQuery.DateModification.Millisecond);
+                DateTime dmd = jQuery.DateModification;             
+                jQuery.StateId = db.hState.First(m => m.Description == "Ожидает").StateId;
+                jQuery.PersonId = User.Identity.GetUserId();
+                db.jQuery.Add(jQuery);
+                db.SaveChanges();
+
+                jJournal jJur = new jJournal();
+                string dmdstr = dmd.ToString("yyyy-MM-dd HH:mm:ss") + ".000";
+                DateTime dmdn = DateTime.Parse(dmdstr);
+                db = new KOMK_Main_v2Entities();
+                jQuery jQ = db.jQuery.First(m => m.DateModification== dmdn);    
+
+                jJur.Date= DateTime.Now;
+                jJur.EventTypeId = db.hEventType.First(m => m.Description == "Создание заявки").EventTypeId; 
+                jJur.WorkListId = null;                
+                jJur.PersonId = User.Identity.GetUserId();
+                jJur.QueryID = jQ.QueryId;
+                jJur.Description = " Содержание:  "+jQ.Text;
+
+
+                db.jJournal.Add(jJur);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            //Если данные не валидны то делаем тоже что и в методе GET
+            ViewBag.PersonId = new SelectList(db.AspNetUsers, "Id", "Fio", jQuery.PersonId);
+            ViewBag.PersonSpId = new SelectList(db.AspNetUsers, "Id", "Fio", jQuery.PersonSpId);
+            ViewBag.StateId = new SelectList(db.hState, "StateId", "Description", jQuery.StateId);
+            return View(jQuery);
+        }
+
+
+
+        // GET: jQueries/ChangeStatusQuery/5
+        // Метод Смена статуса заявки, имеет аргумент id, чтобы производить действия над конкретной заявкой
+        public ActionResult ChangeStatusQuery(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            jQuery jQuery = db.jQuery.Find(id);
+            TempData["oldState"] = jQuery.StateId;
+            if (jQuery == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PersonId = new SelectList(db.AspNetUsers, "Id", "Fio", jQuery.PersonId);
+            ViewBag.PersonSpId = new SelectList(db.AspNetUsers, "Id", "Fio", jQuery.PersonSpId);
+            ViewBag.StateId = new SelectList(db.hState, "StateId", "Description", jQuery.StateId);
+            return View(jQuery);
+        }
+
+        // POST: jQueries/ChangeStatusQuery/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost] // Сохраняем изменения 
+        [ValidateAntiForgeryToken]
+        public ActionResult ChangeStatusQuery([Bind(Include = "QueryId,DateOut,DateIn,DateModification,DeadLine,Text,StateId,PersonId,PersonSpId")] jQuery jQuery)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jQuery).State = EntityState.Modified;
+                if (jQuery.StateId == db.hState.First(m => m.Description == "Выполнена").StateId || jQuery.StateId == db.hState.First(m => m.Description == "Отклонена").StateId)
+                {
+                    jQuery.DateOut= DateTime.Parse(DateTime.Today.ToShortDateString());
+                }
+                if (Convert.ToInt32(TempData["oldState"])!= jQuery.StateId) {
+                    jJournal jJur = new jJournal();
+                    jJur.Date = DateTime.Now;
+                    jJur.EventTypeId = db.hEventType.First(m => m.Description == "Смена статуса заявки").EventTypeId;
+                    jJur.WorkListId = null;
+                    jJur.PersonId = User.Identity.GetUserId();
+                    jJur.QueryID = jQuery.QueryId;
+                    hState oldst = db.hState.Find(Convert.ToInt32(TempData["oldState"]));
+                    hState newst = db.hState.Find(jQuery.StateId);
+                    jJur.Description = "c " + oldst.Description + " на " + newst.Description;
+                    db.jJournal.Add(jJur);
+                }
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PersonId = new SelectList(db.AspNetUsers, "Id", "Fio", jQuery.PersonId);
+            ViewBag.PersonSpId = new SelectList(db.AspNetUsers, "Id", "Fio", jQuery.PersonSpId);
+            ViewBag.StateId = new SelectList(db.hState, "StateId", "Description", jQuery.StateId);
+            return View(jQuery);
+        }
+
+        // GET: jQueries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            jQuery jQuery = db.jQuery.Find(id);
+            if (jQuery == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PersonId = new SelectList(db.AspNetUsers, "Id", "Fio", jQuery.PersonId);
+            ViewBag.PersonSpId = new SelectList(db.AspNetUsers, "Id", "Fio", jQuery.PersonSpId);
+            ViewBag.StateId = new SelectList(db.hState, "StateId", "Description", jQuery.StateId);
+            return View(jQuery);
+        }
+
+        // POST: jQueries/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "QueryId,DateOut,DateIn,DateModification,DeadLine,Text,StateId,PersonId,PersonSpId")] jQuery jQuery)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jQuery).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PersonId = new SelectList(db.AspNetUsers, "Id", "Fio", jQuery.PersonId);
+            ViewBag.PersonSpId = new SelectList(db.AspNetUsers, "Id", "Fio", jQuery.PersonSpId);
+            ViewBag.StateId = new SelectList(db.hState, "StateId", "Description", jQuery.StateId);
+            return View(jQuery);
+        }
+        // GET: jQueries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            jQuery jQuery = db.jQuery.Find(id);
+            if (jQuery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jQuery);
+        }
+
+        // POST: jQueries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            jQuery jQuery = db.jQuery.Find(id);
+            db.jQuery.Remove(jQuery);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        public ActionResult AjaxTest()
+        {
+            ViewData["data"] = Request["data"];
+            if (Request.IsAjaxRequest())
+                return PartialView("_AjaxTestPartial");
+            return View();
+        }
+        public double MyPow(int a)
+        {
+            return Math.Pow(a, 2);
+        }
+
+        public ActionResult SomeText()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View("SomeText");
+        }
+    }
+}
